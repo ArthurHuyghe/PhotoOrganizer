9c628383259a72e343c0bd29b4bbbb76ed62a305
ArthurHuyghe <117371534+ArthurHuyghe@users.noreply.github.com>
2025-09-18 11:08:50 +0200

    Remove quick import gui.py from tracking

--- Playground/quick import gui.py
+++
@@ -1,9 +0,0 @@
-from PyQt6 import QtWidgets, uic
-import sys
-
-app = QtWidgets.QApplication(sys.argv)
-# app.setStyle("Fusion")  # optional: unify with Designer look
-
-window = uic.loadUi("src/assets/progressWindow - kopie.ui")
-window.show()
-app.exec()
